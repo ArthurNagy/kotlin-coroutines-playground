67ad3072b1e05e4e69d4051586658c453ca495dd
Arthur Nagy <arthur.93.nagy@gmail.com>
2018-11-14 00:15:04 +0200

    Implement custom night & day theme

--- app/src/main/java/me/arthurnagy/news/App.kt
+++ app/src/main/java/me/arthurnagy/news/App.kt
@@ -1,5 +1,14 @@
 package me.arthurnagy.news
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 
-class App : Application()
+class App : Application() {
+
+    companion object {
+        init {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO)
+        }
+    }
+
+}
